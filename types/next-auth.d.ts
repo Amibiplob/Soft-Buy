700c9c047ede33d71f016f6fb2b9ca28771a2e78
amibiplob <103816819+Amibiplob@users.noreply.github.com>
2026-08-27 19:34:25 +0600

    role model change user to buyer

--- types/next-auth.d.ts
+++ types/next-auth.d.ts
@@ -1,15 +1,22 @@
-import NextAuth, { DefaultSession } from "next-auth";
+import "next-auth";
+import "next-auth/jwt";
 
 declare module "next-auth" {
   interface Session {
     user: {
       id: string;
-      role: string;
+      role: "buyer" | "seller";
     } & DefaultSession["user"];
   }
-
   interface User {
     id: string;
-    role: string;
+    role: "buyer" | "seller";
+  }
+}
+
+declare module "next-auth/jwt" {
+  interface JWT {
+    id: string;
+    role: "buyer" | "seller";
   }
 }
